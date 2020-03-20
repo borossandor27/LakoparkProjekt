31782001cd6dda9682b78b9b02cf66974cf85661
borossandor27 <boros.sanyi@gmail.com>
2020-03-20 16:11:21 +0100

    Add project files.

---
+++ LakoparkProjekt/HappyLiving.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LakoparkProjekt
+{
+    class HappyLiving
+    {
+        List<Lakopark> lakoparkok = new List<Lakopark>();
+
+        internal List<Lakopark> Lakoparkok { get => lakoparkok; set => lakoparkok = value; }
+
+        public HappyLiving(string filenev)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filenev))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string LakoparkNev = sr.ReadLine();
+                        string[] sor = sr.ReadLine().Split(';');
+                        int utcakSzama = int.Parse(sor[0]);
+                        int hazakSzama = int.Parse(sor[1]);
+                        int[,] hazak = new int[utcakSzama, hazakSzama];
+                        string ujSor = string.Empty;
+                        while (true)
+                        {
+                            ujSor = sr.ReadLine();
+                            if (ujSor.Trim().Length < 1)
+                            {
+                                break;
+                            }
+                            sor = ujSor.Split(';');
+                            hazak[int.Parse(sor[0]) - 1, int.Parse(sor[1]) - 1] = int.Parse(sor[2]);
+                        }
+                        lakoparkok.Add(new Lakopark(LakoparkNev, utcakSzama, hazakSzama, hazak));
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ LakoparkProjekt/Lakopark.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LakoparkProjekt
+{
+    class Lakopark
+    {
+        int[,] hazak;
+        int maxHazSzam;
+        string nev;
+        int utcakSzama;
+
+        public int[,] Hazak { get => hazak; set => hazak = value; }
+        public int MaxHazSzam { get => maxHazSzam; set => maxHazSzam = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public int UtcakSzama { get => utcakSzama; set => utcakSzama = value; }
+
+        public Lakopark(string nev, int utcakSzama, int maxHazSzam, int[,] hazak)
+        {
+            this.hazak = hazak;
+            this.maxHazSzam = maxHazSzam;
+            this.nev = nev;
+            this.utcakSzama = utcakSzama;
+        }
+    }
+}
